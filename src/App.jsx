1a33cd5f5ba075aaa42d6ad8c995db0551b8fdf2
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-04-17 02:03:58 +0100

    Add 'AddTask' component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Header from './components/Header';
+import AddTasks from './components/AddTasks';
 
 function App(){
     return (
         <div className='main' > 
             <Header />
-
+            <AddTasks />
         </div>
     );
 }
--- src/components/AddTasks.jsx
+++ src/components/AddTasks.jsx
@@ -1,1 +1,24 @@
+import { useState } from "react";
+import tasks from "../utils";
 
+function AddTasks(){
+    const [text, setText] = useState("");
+
+    const saveText = (newText) => {
+        setText(newText);
+    }
+
+    const addNewTask = () => {
+        tasks.push(text);
+        setText("");
+    }
+
+    return (
+        <div className="add-tasks">
+            <input className="add-tasks-input" value={text} type="text" onChange={(e) => {saveText(e.target.value)}} />
+            <button className="add-tasks-button" onClick={addNewTask}>Add</button>
+        </div>
+    )
+}
+
+export default AddTasks;
