5f363e0db278a8aafe0f9db153b13d7475b3faff
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-04-17 18:29:55 +0100

    fix the import in the 'AddTasks.jsx' file

--- src/components/AddTasks.jsx
+++ src/components/AddTasks.jsx
@@ -1,28 +1,33 @@
-import { useState } from "react";
-import tasks from "../utils";
+import { useState } from 'react';
 
-function AddTasks(props){
-    
-
-    const [text, setText] = useState("");
+function AddTasks(props) {
+    const [text, setText] = useState('');
 
     const saveText = (newText) => {
         setText(newText);
-    }
+    };
 
-    
     const addNewTask = () => {
         const newId = props.value.length > 0 ? props.value[props.value.length - 1].id + 1 : 1;
-        props.setValue([...props.value, {id: newId, title: text}]);
-        setText("");
-    }
+        props.setValue([...props.value, { id: newId, title: text }]);
+        setText('');
+    };
 
     return (
         <div className="add-tasks">
-            <input className="add-tasks-input" value={text} type="text" onChange={(e) => {saveText(e.target.value)}} />
-            <button className="add-tasks-button" onClick={addNewTask}>Add</button>
+            <input
+                className="add-tasks-input"
+                value={text}
+                type="text"
+                onChange={(e) => {
+                    saveText(e.target.value);
+                }}
+            />
+            <button className="add-tasks-button" onClick={addNewTask}>
+                Add
+            </button>
         </div>
-    )
+    );
 }
 
 export default AddTasks;
