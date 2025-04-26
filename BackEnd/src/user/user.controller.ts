2309e4306c7af948a09835fee7c9a0fdfcb88f17
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-04-26 23:28:18 +0100

    feat(user): implement basic signup endpoint

--- BackEnd/src/user/user.controller.ts
+++ BackEnd/src/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService) {}
+
+    @Post('/signup')
+    signup(@Body() userData: any) {
+        return this.userService.signup(userData);
+    }
+
+    @Get()
+    getHello(): string {
+        return 'Hello bro!';
+    }
+}
--- BackEnd/src/user/user.service.ts
+++ BackEnd/src/user/user.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/typeorm/entities/User';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ) {}
+
+    signup(userData: any) {
+        // Create a new user
+        const newUser = this.userRepository.create(userData);
+
+        // Save the user to the database
+        return this.userRepository.save(newUser);
+    }
+}
