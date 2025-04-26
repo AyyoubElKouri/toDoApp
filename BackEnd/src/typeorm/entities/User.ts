9e312279e62ea9ee06ec8b45a44495d106ba2a12
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-04-26 23:05:22 +0100

    feat(typeorm): add typeorm configuration

---
+++ BackEnd/src/typeorm/entities/User.ts
@@ -0,0 +1,14 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+}
