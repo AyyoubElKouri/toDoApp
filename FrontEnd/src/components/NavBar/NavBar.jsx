27c1b86cc28b5a008ffda0756122acf435df87d0
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-04-28 18:00:30 +0100

    refactor(NavBar): add styling, and clean the code

--- FrontEnd/src/components/NavBar/NavBar.jsx
+++ FrontEnd/src/components/NavBar/NavBar.jsx
@@ -1,18 +1,26 @@
-import { Link } from "react-router";
-import './NavBar.css'
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 import logo from '../../assets/toDoIcon.png';
 
-function NavBar(){
+function NavBar() {
     return (
-        <div className="navbar-contaienr">
-            <img class="navbar-logo" src={logo} alt="Error" />
-            <h2 className="navbar-title">Welcome to our To Do System</h2>
-            <ul className="navbar-items">
-                <li className="navbar-item"><Link to='/'>Home</Link></li>
-                <li className="navbar-item"><Link to='/Welcome'>Welcome</Link></li>
+        <div className="navbar__container">
+            <img className="navbar__logo" src={logo} alt="To Do App Logo" />
+            <h2 className="navbar__title">Welcome to our To Do System</h2>
+            <ul className="navbar__items">
+                <li className="navbar__item-container">
+                    <Link className="navbar__item" to="/">
+                        Home
+                    </Link>
+                </li>
+                <li className="navbar__item-container">
+                    <Link className="navbar__item" to="/Welcome">
+                        Welcome
+                    </Link>
+                </li>
             </ul>
         </div>
-    )
+    );
 }
 
 export default NavBar;
