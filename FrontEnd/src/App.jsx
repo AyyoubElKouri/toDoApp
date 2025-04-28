702e6a9b4afce90a8a54bac11b50bff61438d441
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-04-28 16:37:41 +0100

    feat(App): add routing system

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
-import Header from './components/Header';
-import AddTasks from './components/AddTasks';
-import Tasks from './components/Tasks';
+import Header from './components/Header/Header';
+import AddTasks from './components/AddTasks/AddTasks';
+import Tasks from './components/Tasks/Tasks';
 import { useState } from 'react';
 
 function App(){
     const [task, setTask] = useState([]);
     return (
-        <div className='main' > 
+        <div className='main' >
             <Header />
             <AddTasks value={task} setValue={setTask}/>
             <Tasks value={task} setValue={setTask} />
--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+
+import './index.css';
+import App from './App.jsx';
+import Welcome from './pages/Welcome.jsx';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />
+    },
+    {
+        path: '/welcome',
+        element: <Welcome />
+    }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
+);
